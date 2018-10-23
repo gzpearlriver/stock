d8dcd3cecc67c94dc55a02a47b7e03b1fce9095d
Francis Lu <frank@139.com>
2018-10-23 06:45:28 -0700

    add update stock list

--- update_stocklist.py
+++ update_stocklist.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#allow chinese character in program
+
 import pandas as pd
 import sqlite3 as lite
 import tushare as ts
 
 
-db_file = 'd:\\stockdata\\stock.db'
+db_file = '/code/stock/stock.db'
 
 conn=lite.connect(db_file)
+conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+#allow chinese character in database
 cur = conn.cursor()
 
 sql = "select * from stocklist"
@@ -15,7 +21,7 @@ newlist = ts.get_stock_basics()
 newlist['name'] = newlist['name'].str.replace('*','')
 newlist['name'] = newlist['name'].str.replace(' ','')
 newlist = newlist.reset_index()
-#把code从Index转为列
+
 oldlist['code'] = oldlist['code'].astype(str)
 oldlist['code'] = oldlist['code'].str.zfill(6)
 
@@ -27,6 +33,9 @@ delta_o_n = set(oldlist['code']) - set(newlist['code'])
 print("old - new")
 print(delta_o_n)
 
+cur.execute('drop table stocklist_old;')
+cur.execute('drop index stocklist_old;')
 cur.execute('alter table stocklist rename to stocklist_old;')
+cur.execute('drop index stocklist;')
 newlist.to_sql('stocklist', con=conn, if_exists='append')
 
