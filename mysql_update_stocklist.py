888a389ebf1dc597bd39a7132720808186555aca
gzpearlriver <frank@139.com>
2020-01-17 04:13:43 -0500

    change server and domain name

--- mysql_update_stocklist.py
+++ mysql_update_stocklist.py
@@ -12,7 +12,8 @@ import sys
 import tushare as ts
 
 
-engine = sqlalchemy.create_engine("mysql+pymysql://stock:stock@104.225.154.46:3306/stock?use_unicode=1&charset=utf8",encoding='utf-8',echo=False,max_overflow=5)
+engine = sqlalchemy.create_engine("mysql+mysqlconnector://stock:stock@stock.riverriver.xyz:3306/stock?use_unicode=1&charset=utf8",encoding='utf-8',echo=False,max_overflow=5)
+#engine = sqlalchemy.create_engine("mysql+mysqlconnector://stock:stock@66.98.125.27:3306/stock?use_unicode=1&charset=utf8",encoding='utf-8',echo=False,max_overflow=5)
 metadata = sqlalchemy.MetaData(engine)
 conn_mysql = engine.connect()
 #stockdata = sqlalchemy.Table('stockdata', metadata, autoload=True, autoload_with=engine)
@@ -25,81 +26,97 @@ print(oldlist.iloc[0:3,:])
 del oldlist['index']
 
 tslist = ts.get_stock_basics()
-tslist = tslist.reset_index()
-newlist = pd.DataFrame()
-newlist['name'] = tslist['name']
-newlist['code'] = tslist['code']
-newlist['industry'] = tslist['industry']
-newlist['area'] = tslist['area']
-newlist['name'] = newlist['name'].str.replace('*','')
-newlist['name'] = newlist['name'].str.replace(' ','')
 
-
-print(newlist.columns)
-print(newlist.iloc[0:3,:])
-
-
-oldcode = oldlist['code'].values.tolist()
-newcode = newlist['code'].values.tolist()
-#print(newcode)
-#print(oldcode)
-
-delta_n_o = set(newcode) - set(oldcode)
-print("new - old")
-print(delta_n_o)
-
-delta_o_n = set(oldcode) - set(newcode)
-print("old - new")
-print(delta_o_n)
-
-same_n_o = set(oldcode) & set(newcode)
-print(len(same_n_o))
-
-new_stock = newlist[newlist['code'].isin(delta_n_o)]
-new_stock['new'] = True
-new_stock['trading'] = True
-new_stock['lastread163'] = date(2019,1,1)
-new_stock['seq'] = 0
-
-old_stock = oldlist[oldlist['code'].isin(same_n_o)]
-old_stock['new'] = False
-old_stock['trading'] = True
-
-off_stock = oldlist[oldlist['code'].isin(delta_o_n)]
-off_stock['new'] = False
-off_stock['trading'] = False
-
-stocklist = pd.concat([new_stock, old_stock, off_stock], ignore_index=True, sort=True)
-
-
-print(stocklist.columns,len(stocklist))
-print(stocklist.iloc[0:3,:])
-
-stocklist.to_sql('stocklist', con=conn_mysql, if_exists='replace')
-'''
-try:
-    cur.execute('drop index stocklist_old;')
-except:
-    print('drop index stocklist_old fail')
+if len(tslist)>0:
+    tslist = tslist.reset_index()
+    newlist = pd.DataFrame()
+    newlist['name'] = tslist['name']
+    newlist['code'] = tslist['code']
+    newlist['industry'] = tslist['industry']
+    newlist['area'] = tslist['area']
+    newlist['name'] = newlist['name'].str.replace('*','')
+    newlist['name'] = newlist['name'].str.replace(' ','')
     
-try:
-    cur.execute('drop table stocklist_old;')
-except:
-    print('drop table stocklist_old fail')
+    print(newlist.columns)
+    print(newlist.iloc[0:3,:])
     
-try:
-    cur.execute('alter table stocklist rename to stocklist_old;')
-except:
-    print('alter table stocklist rename fail')
     
-try:
-    cur.execute('drop index stocklist;')
-except:
-    print('drop index stocklist fail')
+    oldcode = oldlist['code'].values.tolist()
+    newcode = newlist['code'].values.tolist()
+    #print(newcode)
+    #print(oldcode)
     
-try:
-    newlist.to_sql('stocklist', con=conn, if_exists='append')
-except:
-    print('insert stocklist fail')'''
+    delta_n_o = set(newcode) - set(oldcode)
+    print("new - old")
+    print(len(delta_n_o))
     
+    delta_o_n = set(oldcode) - set(newcode)
+    print("old - new")
+    print(len(delta_o_n))
+    
+    same_n_o = set(oldcode) & set(newcode)
+    print(len(same_n_o))
+    
+    #筛选新股票
+    new_stock = newlist[newlist['code'].isin(delta_n_o)]
+    new_stock.loc[:, 'new'] = True
+    new_stock.loc[:, 'trading'] = True
+    new_stock.loc[:, 'lastread163'] = date.today()
+    new_stock.loc[:, 'seq'] = 0
+    
+    #筛选旧股票
+    old_stock = oldlist[oldlist['code'].isin(same_n_o)]
+    old_stock.loc[:, 'new'] = True
+    old_stock.loc[:, 'trading'] = True
+
+    
+    #筛选退市股票
+    off_stock = oldlist[oldlist['code'].isin(delta_o_n)]
+    old_stock.loc[:, 'new'] = False
+    old_stock.loc[:, 'trading'] = False
+    
+    stocklist = pd.concat([new_stock, old_stock, off_stock], ignore_index=True, sort=True)
+    
+    print(stocklist.columns,len(stocklist))
+    print(stocklist.iloc[0:3,:])
+ 
+    try:
+        cur.execute('drop index stocklist_old;')
+    except:
+        print('drop index stocklist_old fail')
+        
+    try:
+        cur.execute('alter table stocklist rename to stocklist_old;')
+    except:
+        print('alter table stocklist rename fail')
+
+    stocklist.to_sql('stocklist', con=conn_mysql, if_exists='replace')
+        
+    '''
+    try:
+        cur.execute('drop index stocklist_old;')
+    except:
+        print('drop index stocklist_old fail')
+        
+    try:
+        cur.execute('drop table stocklist_old;')
+    except:
+        print('drop table stocklist_old fail')
+        
+    try:
+        cur.execute('alter table stocklist rename to stocklist_old;')
+    except:
+        print('alter table stocklist rename fail')
+        
+    try:
+        cur.execute('drop index stocklist;')
+    except:
+        print('drop index stocklist fail')
+        
+    try:
+        newlist.to_sql('stocklist', con=conn, if_exists='append')
+    except:
+        print('insert stocklist fail')
+    '''
+        
 conn_mysql.close()
